a3072c80ca1a6b45b46838a163e8c64d4f97b78d
Yuki Kishimoto <yukikishimoto@proton.me>
2022-10-23 11:02:17 +0200

    Add level arg to init_logger

--- bindings/nostr-sdk-ffi/src/logger.rs
+++ bindings/nostr-sdk-ffi/src/logger.rs
@@ -1,19 +1,31 @@
 // Copyright (c) 2022 Yuki Kishimoto
 // Distributed under the MIT software license
 
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+use std::str::FromStr;
+
 use log::Level;
 
 #[cfg(target_os = "android")]
 use android_logger::Config;
 
-pub fn init_logger() {
-    #[allow(unused_variables)]
-    let min_level = if cfg!(debug_assertions) {
+pub fn init_logger(level: Option<String>) {
+    #[cfg(target_os = "android")]
+    android_logger::init_once(Config::default().with_min_level(min_level(level)));
+}
+
+fn min_level(level: Option<String>) -> Level {
+    if let Some(level_str) = level {
+        if let Ok(level) = Level::from_str(&level_str) {
+            return level;
+        }
+    }
+
+    if cfg!(debug_assertions) {
         Level::Debug
     } else {
         Level::Info
-    };
-
-    #[cfg(target_os = "android")]
-    android_logger::init_once(Config::default().with_min_level(min_level));
+    }
 }
