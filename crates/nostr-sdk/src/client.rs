ed6116e83f560e9c7e3f238844f83b9e82376394
Yuki Kishimoto <yukikishimoto@proton.me>
2022-11-09 11:06:38 +0100

    Update methods docs

--- crates/nostr-sdk/src/client.rs
+++ crates/nostr-sdk/src/client.rs
@@ -26,14 +26,17 @@ impl Client {
         }
     }
 
+    /// Generate new random keys using entorpy from OS
     pub fn generate_keys() -> Keys {
         Keys::generate_from_os_random()
     }
 
+    /// Get new notification listener
     pub fn notifications(&self) -> broadcast::Receiver<RelayPoolNotifications> {
         self.pool.notifications()
     }
 
+    /// Add new relay
     pub fn add_relay(&mut self, url: &str, proxy: Option<SocketAddr>) -> Result<()> {
         self.pool.add_relay(url, proxy)
     }
@@ -41,19 +44,22 @@ impl Client {
 
 #[cfg(not(feature = "blocking"))]
 impl Client {
+    /// Disconnect and remove relay
     pub async fn remove_relay(&mut self, url: &str) -> Result<()> {
         self.pool.remove_relay(url).await
     }
 
+    /// Connect relay
     pub async fn connect_relay(&mut self, url: &str) -> Result<()> {
         self.pool.connect_relay(url).await
     }
 
+    /// Disconnect relay
     pub async fn disconnect_relay(&mut self, url: &str) -> Result<()> {
         self.pool.disconnect_relay(url).await
     }
 
-    /// Connect to relays and keep connection alive
+    /// Connect to all added relays and keep connection alive
     pub async fn connect(&mut self) -> Result<()> {
         self.pool.connect().await
     }
@@ -63,14 +69,17 @@ impl Client {
         self.pool.disconnect().await
     }
 
+    /// Subscribe to filters
     pub async fn subscribe(&mut self, filters: Vec<SubscriptionFilter>) -> Result<()> {
         self.pool.subscribe(filters).await
     }
 
+    /// Send event
     pub async fn send_event(&self, event: Event) -> Result<()> {
         self.pool.send_event(event).await
     }
 
+    /// Delete event
     pub async fn delete_event(&self, event_id: &str) -> Result<()> {
         let event = Event::delete(&self.keys, vec![Hash::from_str(event_id)?], None)?;
         self.send_event(event).await
@@ -92,31 +101,42 @@ impl Client {
 
 #[cfg(feature = "blocking")]
 impl Client {
+    /// Disconnect and remove relay
     pub fn remove_relay(&mut self, url: &str) -> Result<()> {
         RUNTIME.block_on(async { self.pool.remove_relay(url).await })
     }
 
+    /// Connect relay
     pub fn connect_relay(&mut self, url: &str) -> Result<()> {
         RUNTIME.block_on(async { self.pool.connect_relay(url).await })
     }
 
+    /// Disconnect relay
     pub fn disconnect_relay(&mut self, url: &str) -> Result<()> {
         RUNTIME.block_on(async { self.pool.disconnect_relay(url).await })
     }
 
-    /// Connect to all disconnected relays
+    /// Connect to all added relays and keep connection alive
     pub fn connect(&mut self) -> Result<()> {
         RUNTIME.block_on(async { self.pool.connect().await })
     }
 
+    /// Disconnect from all relays
+    pub fn disconnect(&mut self) -> Result<()> {
+        RUNTIME.block_on(async { self.pool.disconnect().await })
+    }
+
+    /// Subscribe to filters
     pub fn subscribe(&mut self, filters: Vec<SubscriptionFilter>) -> Result<()> {
         RUNTIME.block_on(async { self.pool.subscribe(filters).await })
     }
 
+    /// Send event
     pub fn send_event(&self, event: Event) -> Result<()> {
         RUNTIME.block_on(async { self.pool.send_event(event).await })
     }
 
+    /// Delete event
     pub fn delete_event(&self, event_id: &str) -> Result<()> {
         let event = Event::delete(&self.keys, vec![Hash::from_str(event_id)?], None)?;
         self.send_event(event)
