d1b5f49b6522a70dba28a2aa5c1bae1d3506814a
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-02-02 14:48:50 +0100

    sdk: update `thread` module

--- crates/nostr-sdk/src/thread.rs
+++ crates/nostr-sdk/src/thread.rs
@@ -15,7 +15,28 @@ fn new_current_thread() -> nostr::Result<Runtime> {
     Ok(Builder::new_current_thread().enable_all().build()?)
 }
 
-pub fn spawn<T>(future: T)
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Impossible to join thread")]
+    JoinError,
+}
+
+#[allow(dead_code)]
+pub enum JoinHandle<T> {
+    Std(std::thread::JoinHandle<T>),
+    Tokio(tokio::task::JoinHandle<T>),
+}
+
+impl<T> JoinHandle<T> {
+    pub async fn join(self) -> Result<T, Error> {
+        match self {
+            Self::Std(handle) => handle.join().map_err(|_| Error::JoinError),
+            Self::Tokio(handle) => handle.await.map_err(|_| Error::JoinError),
+        }
+    }
+}
+
+pub fn spawn<T>(future: T) -> Option<JoinHandle<T::Output>>
 where
     T: Future + Send + 'static,
     T::Output: Send + 'static,
@@ -23,14 +44,22 @@ where
     #[cfg(feature = "blocking")]
     match new_current_thread() {
         Ok(rt) => {
-            std::thread::spawn(move || {
-                rt.block_on(future);
+            let handle = std::thread::spawn(move || {
+                let res = rt.block_on(future);
                 rt.shutdown_timeout(Duration::from_millis(100));
+                res
             });
+            Some(JoinHandle::Std(handle))
         }
-        Err(e) => log::error!("Impossible to create new thread: {:?}", e),
-    };
+        Err(e) => {
+            log::error!("Impossible to create new thread: {:?}", e);
+            None
+        }
+    }
 
     #[cfg(not(feature = "blocking"))]
-    tokio::task::spawn(future);
+    {
+        let handle = tokio::task::spawn(future);
+        Some(JoinHandle::Tokio(handle))
+    }
 }
