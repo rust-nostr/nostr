8c95bf23131d09b4cd72264c66707117f4869e06
Szilard Parrag <szilard.parrag@gmail.com>
2023-04-27 16:00:07 +0200

    nostr/nips: add NIP-58 Badge Definition event

--- crates/nostr/src/nips/nip58.rs
+++ crates/nostr/src/nips/nip58.rs
@@ -1,3 +1,108 @@
 //! NIP58
 //!
 //! <https://github.com/nostr-protocol/nips/blob/master/58.md>
+
+use crate::{event::builder::Error, Event, EventBuilder, Keys, Kind, Tag};
+
+/// Simple struct to hold `width` x `height.
+pub struct ImageDimensions(u64, u64);
+
+/// [`BadgeDefinition`] event builder
+pub struct BadgeDefinitionBuilder {
+    badge_id: String,
+    name: Option<String>,
+    image: Option<String>,
+    image_dimensions: Option<ImageDimensions>,
+    description: Option<String>,
+    thumbs: Option<Vec<(String, Option<ImageDimensions>)>>,
+}
+
+impl BadgeDefinitionBuilder {
+    /// New [`BadgeDefinitionBuilder`]
+    pub fn new(badge_id: String) -> Self {
+        Self {
+            badge_id,
+            name: None,
+            image: None,
+            image_dimensions: None,
+            description: None,
+            thumbs: None,
+        }
+    }
+
+    /// Set name
+    pub fn name(mut self, name: String) -> Self {
+        self.name = Some(name);
+        self
+    }
+
+    /// Set image
+    pub fn image(mut self, image: String) -> Self {
+        self.image = Some(image);
+        self
+    }
+
+    /// Set `[ImageDimensions]`
+    pub fn image_dimensions(mut self, image_dimensions: ImageDimensions) -> Self {
+        self.image_dimensions = Some(image_dimensions);
+        self
+    }
+
+    /// Set description
+    pub fn description(mut self, description: String) -> Self {
+        self.description = Some(description);
+        self
+    }
+
+    /// Set thumbnails with their optional `[ImageDimensions]`
+    pub fn thumbs(mut self, thumbs: Vec<(String, Option<ImageDimensions>)>) -> Self {
+        self.thumbs = Some(thumbs);
+        self
+    }
+
+    /// Build [`Event`]
+    pub fn build(self, keys: &Keys) -> Result<BadgeDefinition, BuilderError> {
+        let mut tags: Vec<Tag> = Vec::new();
+        let badge_id = Tag::Identifier(self.badge_id);
+        tags.push(badge_id);
+
+        if let Some(name) = self.name {
+            let name_tag = Tag::Name(name);
+            tags.push(name_tag);
+        };
+
+        if let Some(image) = self.image {
+            let image_tag = if let Some(width_height) = self.image_dimensions {
+                let ImageDimensions(width, height) = width_height;
+                Tag::Image(image, Some((width, height)))
+            } else {
+                Tag::Image(image, None)
+            };
+            tags.push(image_tag);
+        }
+
+        if let Some(description) = self.description {
+            let description_tag = Tag::Description(description);
+            tags.push(description_tag);
+        }
+        if let Some(thumbs) = self.thumbs {
+            for thumb in thumbs {
+                let thumb_url = thumb.0;
+                let thumb_tag = if let Some(width_height) = thumb.1 {
+                    let ImageDimensions(width, height) = width_height;
+                    Tag::Thumb(thumb_url, Some((width, height)))
+                } else {
+                    Tag::Thumb(thumb_url, None)
+                };
+                tags.push(thumb_tag);
+            }
+        }
+
+        let event_builder = EventBuilder::new(Kind::BadgeDefinition, String::new(), &tags);
+        let event = event_builder.to_event(keys)?;
+        Ok(BadgeDefinition(event))
+    }
+}
+
+/// Badge definition event as specified in NIP-58
+pub struct BadgeDefinition(Event);
