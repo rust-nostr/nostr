6b3ae771a4de8fc9601b1a6ace62fb6f5f85dcc7
Szilard Parrag <szilard.parrag@gmail.com>
2023-05-05 14:33:33 +0200

    nostr/event/tag: allow `a` tags to lack relay URL

--- crates/nostr/src/event/tag.rs
+++ crates/nostr/src/event/tag.rs
@@ -284,7 +284,7 @@ pub enum Tag {
         kind: Kind,
         public_key: XOnlyPublicKey,
         identifier: String,
-        relay_url: UncheckedUrl,
+        relay_url: Option<UncheckedUrl>,
     },
     Relay(UncheckedUrl),
     ContactList {
@@ -402,7 +402,21 @@ where
             }
         } else if tag_len == 2 {
             let content: &str = &tag[1];
+
             match tag_kind {
+                TagKind::A => {
+                    let kpi: Vec<&str> = tag[1].split(':').collect();
+                    if kpi.len() == 3 {
+                        Ok(Self::A {
+                            kind: Kind::from_str(kpi[0])?,
+                            public_key: XOnlyPublicKey::from_str(kpi[1])?,
+                            identifier: kpi[2].to_string(),
+                            relay_url: None,
+                        })
+                    } else {
+                        Err(Error::InvalidLength)
+                    }
+                }
                 TagKind::P => Ok(Self::PubKey(XOnlyPublicKey::from_str(content)?, None)),
                 TagKind::E => Ok(Self::Event(EventId::from_hex(content)?, None, None)),
                 TagKind::R => Ok(Self::Reference(content.to_string())),
@@ -471,7 +485,7 @@ where
                             kind: Kind::from_str(kpi[0])?,
                             public_key: XOnlyPublicKey::from_str(kpi[1])?,
                             identifier: kpi[2].to_string(),
-                            relay_url: UncheckedUrl::from(tag[2].clone()),
+                            relay_url: Some(UncheckedUrl::from(tag[2].clone())),
                         })
                     } else {
                         Err(Error::InvalidLength)
@@ -570,11 +584,16 @@ impl From<Tag> for Vec<String> {
                 public_key,
                 identifier,
                 relay_url,
-            } => vec![
-                TagKind::A.to_string(),
-                format!("{}:{public_key}:{identifier}", kind.as_u64()),
-                relay_url.to_string(),
-            ],
+            } => {
+                let mut vec = vec![
+                    TagKind::A.to_string(),
+                    format!("{}:{public_key}:{identifier}", kind.as_u64()),
+                ];
+                if let Some(relay) = relay_url {
+                    vec.push(relay.to_string());
+                }
+                vec
+            }
             Tag::Relay(url) => vec![TagKind::Relay.to_string(), url.to_string()],
             Tag::ContactList {
                 pk,
@@ -898,7 +917,7 @@ mod tests {
                     "a695f6b60119d9521934a691347d9f78e8770b56da16bb255ee286ddf9fda919"
                 )?,
                 identifier: String::from("ipsum"),
-                relay_url: UncheckedUrl::from("wss://relay.nostr.org")
+                relay_url: Some(UncheckedUrl::from_str("wss://relay.nostr.org")?)
             }
             .as_vec()
         );
@@ -1119,7 +1138,7 @@ mod tests {
                     "a695f6b60119d9521934a691347d9f78e8770b56da16bb255ee286ddf9fda919"
                 )?,
                 identifier: String::from("ipsum"),
-                relay_url: UncheckedUrl::from("wss://relay.nostr.org")
+                relay_url: Some(UncheckedUrl::from_str("wss://relay.nostr.org")?)
             }
         );
 
