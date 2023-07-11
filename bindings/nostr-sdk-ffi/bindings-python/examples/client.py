802742b3647497ddd5234528ac35e8334c13b24f
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-07-11 10:51:35 +0200

    ffi: update `client.py` example

--- bindings/nostr-sdk-ffi/bindings-python/examples/client.py
+++ bindings/nostr-sdk-ffi/bindings-python/examples/client.py
@@ -1,4 +1,5 @@
-from nostr_sdk import Keys, Client, EventBuilder
+from nostr_sdk import Keys, Client, EventBuilder, Filter
+import time
 
 keys = Keys.generate()
 print(keys.public_key_bech32())
@@ -8,7 +9,16 @@ client = Client(keys)
 client.add_relay("wss://relay.damus.io")
 client.connect()
 
+print("Mining a POW text note...")
 event = EventBuilder.new_text_note("Hello from Rust Nostr Python bindings!", []).to_pow_event(keys, 20)
 event_id = client.send_event(event)
 print(f"Event sent: {event_id}")
+
+time.sleep(2.0)
+
+print("Getting events from relays...")
+filter = Filter().authors([keys.public_key()])
+events = client.get_events_of([filter], None)
+for event in events:
+    print(event.as_json())
 
