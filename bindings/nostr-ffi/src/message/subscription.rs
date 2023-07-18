02d0ecc2d7feddeb4e26d10e638e8b02197580c6
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-07-18 10:17:14 +0200

    ffi: improve `Filter`

--- bindings/nostr-ffi/src/message/subscription.rs
+++ bindings/nostr-ffi/src/message/subscription.rs
@@ -4,11 +4,10 @@
 use std::ops::Deref;
 use std::sync::Arc;
 
-use nostr::{EventId, Filter as FilterSdk, Timestamp};
+use nostr::Filter as FilterSdk;
 
-use crate::error::Result;
 use crate::helper::unwrap_or_clone_arc;
-use crate::PublicKey;
+use crate::{EventId, PublicKey, Timestamp};
 
 #[derive(Clone)]
 pub struct Filter {
@@ -41,34 +40,36 @@ impl Filter {
         }
     }
 
-    pub fn id(self: Arc<Self>, id: String) -> Result<Arc<Self>> {
+    pub fn id(self: Arc<Self>, id: String) -> Arc<Self> {
         let mut builder = unwrap_or_clone_arc(self);
         builder.sub_filter = builder.sub_filter.id(id);
-
-        Ok(Arc::new(builder))
+        Arc::new(builder)
     }
 
-    pub fn ids(self: Arc<Self>, ids: Vec<String>) -> Result<Arc<Self>> {
+    pub fn ids(self: Arc<Self>, ids: Vec<String>) -> Arc<Self> {
         let mut builder = unwrap_or_clone_arc(self);
         builder.sub_filter = builder.sub_filter.ids(ids);
-
-        Ok(Arc::new(builder))
+        Arc::new(builder)
     }
 
     pub fn kind(self: Arc<Self>, kind: u64) -> Arc<Self> {
         let mut builder = unwrap_or_clone_arc(self);
         builder.sub_filter = builder.sub_filter.kind(kind.into());
-
         Arc::new(builder)
     }
 
-    pub fn event(self: Arc<Self>, event_id: String) -> Result<Arc<Self>> {
-        let event_id = EventId::from_hex(event_id)?;
-
+    pub fn event(self: Arc<Self>, event_id: Arc<EventId>) -> Arc<Self> {
         let mut builder = unwrap_or_clone_arc(self);
-        builder.sub_filter = builder.sub_filter.events(vec![event_id]);
+        builder.sub_filter = builder.sub_filter.event(event_id.as_ref().into());
+        Arc::new(builder)
+    }
 
-        Ok(Arc::new(builder))
+    pub fn events(self: Arc<Self>, ids: Vec<Arc<EventId>>) -> Arc<Self> {
+        let mut builder = unwrap_or_clone_arc(self);
+        builder.sub_filter = builder
+            .sub_filter
+            .events(ids.into_iter().map(|id| id.as_ref().into()).collect());
+        Arc::new(builder)
     }
 
     pub fn pubkey(self: Arc<Self>, pubkey: Arc<PublicKey>) -> Arc<Self> {
@@ -77,24 +78,29 @@ impl Filter {
         Arc::new(builder)
     }
 
-    pub fn since(self: Arc<Self>, timestamp: u64) -> Arc<Self> {
+    pub fn pubkeys(self: Arc<Self>, pubkeys: Vec<Arc<PublicKey>>) -> Arc<Self> {
         let mut builder = unwrap_or_clone_arc(self);
-        builder.sub_filter = builder.sub_filter.since(Timestamp::from(timestamp));
-
+        builder.sub_filter = builder
+            .sub_filter
+            .pubkeys(pubkeys.into_iter().map(|id| *id.as_ref().deref()).collect());
         Arc::new(builder)
     }
 
-    pub fn until(self: Arc<Self>, timestamp: u64) -> Arc<Self> {
+    pub fn since(self: Arc<Self>, timestamp: Arc<Timestamp>) -> Arc<Self> {
         let mut builder = unwrap_or_clone_arc(self);
-        builder.sub_filter = builder.sub_filter.until(Timestamp::from(timestamp));
-
+        builder.sub_filter = builder.sub_filter.since(*timestamp.as_ref().deref());
         Arc::new(builder)
     }
 
-    pub fn authors(self: Arc<Self>, authors: Vec<String>) -> Result<Arc<Self>> {
+    pub fn until(self: Arc<Self>, timestamp: Arc<Timestamp>) -> Arc<Self> {
+        let mut builder = unwrap_or_clone_arc(self);
+        builder.sub_filter = builder.sub_filter.until(*timestamp.as_ref().deref());
+        Arc::new(builder)
+    }
+
+    pub fn authors(self: Arc<Self>, authors: Vec<String>) -> Arc<Self> {
         let mut builder = unwrap_or_clone_arc(self);
         builder.sub_filter = builder.sub_filter.authors(authors);
-
-        Ok(Arc::new(builder))
+        Arc::new(builder)
     }
 }
