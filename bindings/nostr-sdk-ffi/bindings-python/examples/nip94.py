7581f19ad779b4a8ec3dc4a6276d9992ac07f6f5
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-07-21 14:58:17 +0200

    ffi: add `nip94.py` example

---
+++ bindings/nostr-sdk-ffi/bindings-python/examples/nip94.py
@@ -0,0 +1,19 @@
+from nostr_sdk import Keys, Client, FileMetadata
+
+keys = Keys.generate()
+print(keys.public_key().to_bech32())
+
+client = Client(keys)
+
+client.add_relay("wss://relay.damus.io")
+client.add_relay("ws://jgqaglhautb4k6e6i2g34jakxiemqp6z4wynlirltuukgkft2xuglmqd.onion", "127.0.0.1:9050")
+client.connect()
+
+try:
+    metadata = FileMetadata("https://github.com/coinstr/coinstr/archive/refs/tags/v0.3.0.zip", "application/zip", "3951c152d38317e9ef2c095ddb280613e22b14b166f5fa5950d18773ac0a1d00")
+    event_id = client.file_metadata("Coinstr Alpha Release v0.3.0", metadata)
+    print("Event sent:")
+    print(f" hex:    {event_id.to_hex()}")
+    print(f" bech32: {event_id.to_bech32()}")
+except Exception as e:
+    print(f"{e}")
