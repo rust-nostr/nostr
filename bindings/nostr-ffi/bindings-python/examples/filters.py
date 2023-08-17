5c9b84e481f703ba1f07812386a14e1a0dc541cd
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-08-17 10:04:17 +0200

    ffi: update python examples

--- bindings/nostr-ffi/bindings-python/examples/filters.py
+++ bindings/nostr-ffi/bindings-python/examples/filters.py
@@ -1,9 +1,9 @@
-from nostr_protocol import Filter, Alphabet, Keys
+from nostr_protocol import Filter, Alphabet, Keys, Kind, KindEnum
 
 keys = Keys.generate()
 
-filter = Filter().pubkey(keys.public_key()).kinds([0, 1]).custom_tag(Alphabet.J, ["test"])
+filter = Filter().pubkey(keys.public_key()).kinds([Kind(0), Kind.from_enum(KindEnum.TEXT_NOTE())]).custom_tag(Alphabet.J, ["test"])
 print(filter.as_json())
 
-filter = filter.kind(4).custom_tag(Alphabet.J, ["append-new"])
+filter = filter.kind(Kind.from_enum(KindEnum.ENCRYPTED_DIRECT_MESSAGE())).custom_tag(Alphabet.J, ["append-new"])
 print(filter.as_json())
--- bindings/nostr-sdk-ffi/bindings-python/examples/bot.py
+++ bindings/nostr-sdk-ffi/bindings-python/examples/bot.py
@@ -1,4 +1,4 @@
-from nostr_sdk import Keys, Client, Event, EventBuilder, Filter, HandleNotification, Timestamp, nip04_decrypt, SecretKey, init_logger, LogLevel
+from nostr_sdk import Keys, Client, Event, EventBuilder, Filter, Kind, KindEnum, HandleNotification, Timestamp, nip04_decrypt, SecretKey, init_logger, LogLevel
 import time
 
 init_logger(LogLevel.DEBUG)
@@ -19,13 +19,13 @@ client.add_relay("wss://nostr.mom")
 client.add_relay("wss://nostr.oxtr.dev")
 client.connect()
 
-filter = Filter().pubkey(pk).kind(4).since(Timestamp.now())
+filter = Filter().pubkey(pk).kind(Kind(4)).since(Timestamp.now())
 client.subscribe([filter])
 
 class NotificationHandler(HandleNotification):
     def handle(self, relay_url, event):
         print(f"Received new event from {relay_url}: {event.as_json()}")
-        if event.kind() == 4:
+        if event.kind().as_enum() == KindEnum.ENCRYPTED_DIRECT_MESSAGE():
             print("Decrypting event")
             try:
                 msg = nip04_decrypt(sk, event.pubkey(), event.content())
