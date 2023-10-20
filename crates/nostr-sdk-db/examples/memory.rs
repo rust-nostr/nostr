287cdfa830b33ebd5432f34228f66963bc1412ed
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-10-20 14:15:04 +0200

    db: add `memory` example

---
+++ crates/nostr-sdk-db/examples/memory.rs
@@ -0,0 +1,51 @@
+// Copyright (c) 2022-2023 Yuki Kishimoto
+// Distributed under the MIT software license
+
+use std::time::{Duration, Instant};
+
+use nostr::{EventBuilder, Filter, Keys, Kind, Metadata, Tag};
+use nostr_sdk_db::memory::MemoryDatabase;
+use nostr_sdk_db::NostrDatabase;
+
+#[tokio::main]
+async fn main() {
+    let keys = Keys::generate();
+    let database = MemoryDatabase::new();
+
+    for i in 0..50_000 {
+        let event = EventBuilder::new_text_note(format!("Event #{i}"), &[])
+            .to_event(&keys)
+            .unwrap();
+        database.save_event(&event).await.unwrap();
+
+        let event = EventBuilder::new_text_note(
+            format!("Reply to event #{i}"),
+            &[
+                Tag::Event(event.id, None, None),
+                Tag::PubKey(event.pubkey, None),
+            ],
+        )
+        .to_event(&keys)
+        .unwrap();
+        database.save_event(&event).await.unwrap();
+    }
+
+    for i in 0..10 {
+        let metadata = Metadata::new().name(format!("Name #{i}"));
+        let event = EventBuilder::set_metadata(metadata)
+            .to_event(&keys)
+            .unwrap();
+        database.save_event(&event).await.unwrap();
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+
+    let now = Instant::now();
+    let events = database
+        .query(vec![Filter::new()
+            .kind(Kind::Metadata)
+            .author(keys.public_key().to_string())])
+        .await
+        .unwrap();
+    println!("{events:?}");
+    println!("Time: {} ns", now.elapsed().as_nanos());
+}
