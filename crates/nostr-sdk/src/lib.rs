f3cd63e77790fee804082d31231dfb59c4c4424f
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-11-09 15:22:10 +0100

    Add `nostr-database` to workspace

--- crates/nostr-sdk/src/lib.rs
+++ crates/nostr-sdk/src/lib.rs
@@ -18,12 +18,13 @@
 compile_error!("`blocking` feature can't be enabled for WASM targets");
 
 pub use nostr::{self, *};
+pub use nostr_database as database;
 #[cfg(feature = "indexeddb")]
-pub use nostr_sdk_indexeddb::WebDatabase;
+pub use nostr_indexeddb::WebDatabase;
+#[cfg(feature = "rocksdb")]
+pub use nostr_rocksdb::RocksDatabase;
 #[cfg(feature = "blocking")]
 use nostr_sdk_net::futures_util::Future;
-#[cfg(feature = "rocksdb")]
-pub use nostr_sdk_rocksdb::RocksDatabase;
 #[cfg(feature = "blocking")]
 use once_cell::sync::Lazy;
 #[cfg(feature = "blocking")]
