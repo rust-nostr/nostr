2f620e6fd57339509b61cd8792075a2ee361cb42
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-11-20 09:06:59 +0100

    ffi: fix `nostr-sdk` python aarch64 script

--- bindings/nostr-sdk-ffi/bindings-python/scripts/generate-linux-aarch64.sh
+++ bindings/nostr-sdk-ffi/bindings-python/scripts/generate-linux-aarch64.sh
@@ -6,13 +6,13 @@ pip install -r requirements.txt
 
 echo "Generating nostr_sdk.py..."
 cd ../
-cargo run -p uniffi-bindgen generate src/nostr_sdk.udl --language python --no-format -o bindings-python/src/nostr_sdk/
+cargo run -p uniffi-bindgen generate src/nostr_sdk.udl --language python --no-format -o bindings-python/src/nostr-sdk/
 
 echo "Generating native binaries..."
 rustup target add aarch64-unknown-linux-gnu
 CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc cargo build --release --target aarch64-unknown-linux-gnu
 
 echo "Copying linux libnostr_sdk_ffi.so..."
-cp ../../target/aarch64-unknown-linux-gnu/release/libnostr_sdk_ffi.so bindings-python/src/nostr_sdk/
+cp ../../target/aarch64-unknown-linux-gnu/release/libnostr_sdk_ffi.so bindings-python/src/nostr-sdk/
 
 echo "All done!"
