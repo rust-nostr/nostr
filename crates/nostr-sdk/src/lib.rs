bed0c6c7110585e6b598a38683285f2454472bc5
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-12-17 12:24:02 +0100

    sdk: re-export `Profile`, `NostrDatabase` and `NostrDatabaseExt`

--- crates/nostr-sdk/src/lib.rs
+++ crates/nostr-sdk/src/lib.rs
@@ -19,7 +19,7 @@
 compile_error!("`blocking` feature can't be enabled for WASM targets");
 
 pub use nostr::{self, *};
-pub use nostr_database as database;
+pub use nostr_database::{self as database, NostrDatabase, NostrDatabaseExt, Profile};
 #[cfg(feature = "indexeddb")]
 pub use nostr_indexeddb::{IndexedDBError, WebDatabase};
 #[cfg(feature = "rocksdb")]
