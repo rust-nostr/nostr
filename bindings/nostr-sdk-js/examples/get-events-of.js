1638f3322940f5b92968d4890654e3a4401ffe56
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-12-19 13:53:03 +0100

    js(nostr-sdk): add `get-event-of.js` example

---
+++ bindings/nostr-sdk-js/examples/get-events-of.js
@@ -0,0 +1,24 @@
+const { Keys, Client, Filter, loadWasmAsync } = require("../");
+
+async function main() {
+    await loadWasmAsync();
+
+    let keys = Keys.fromSkStr("nsec1ufnus6pju578ste3v90xd5m2decpuzpql2295m3sknqcjzyys9ls0qlc85");
+
+    let client = new Client(keys);
+    await client.addRelay("wss://relay.damus.io");
+    await client.addRelay("wss://nos.lol");
+    await client.addRelay("wss://nostr.oxtr.dev");
+
+    await client.connect();
+
+    const filter = new Filter().author(keys.publicKey).kind(BigInt(4)).until(BigInt(Date.now())).limit(BigInt(10));
+    console.log('filter', filter.asJson());
+
+    let events = await client.getEventsOf([filter], BigInt(10));
+    events.forEach((e) => {
+        console.log(e.asJson())
+    })
+}
+
+main();
