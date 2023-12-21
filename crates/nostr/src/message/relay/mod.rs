f195c75eae0b16a6b77709531938c5cc18f88d68
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-12-21 20:10:31 +0100

    nostr: update `test_handle_valid_event`

--- crates/nostr/src/message/relay/mod.rs
+++ crates/nostr/src/message/relay/mod.rs
@@ -410,7 +410,7 @@ mod tests {
         let pubkey = "379e863e8357163b5bce5d2688dc4f1dcc2d505222fb8d74db600f30535dfdfe";
         let created_at = Timestamp::from(1612809991);
         let kind = 1;
-        let tags = vec![];
+        let tags = Vec::new();
         let content = "test";
         let sig = "273a9cd5d11455590f4359500bccb7a89428262b96b3ea87a756b770964472f8c3e87f5d5e64d8d2e859a71462a3f477b554565c4f2f326cb01dd7620db71502";
 
@@ -420,6 +420,13 @@ mod tests {
             RelayMessage::from_json(valid_event_msg).unwrap(),
             RelayMessage::new_event(SubscriptionId::new("random_string"), handled_event)
         );
+
+        let message = RelayMessage::from_json(r#"["EVENT","bf7da933d6c6d67e5c97f94f17cf8762",{"content":"Think about this.\n\nThe most powerful centralized institutions in the world have been replaced by a protocol that protects the individual. #bitcoin\n\nDo you doubt that we can replace everything else?\n\nBullish on the future of humanity\nnostr:nevent1qqs9ljegkuk2m2ewfjlhxy054n6ld5dfngwzuep0ddhs64gc49q0nmqpzdmhxue69uhhyetvv9ukzcnvv5hx7un8qgsw3mfhnrr0l6ll5zzsrtpeufckv2lazc8k3ru5c3wkjtv8vlwngksrqsqqqqqpttgr27","created_at":1703184271,"id":"38acf9b08d06859e49237688a9fd6558c448766f47457236c2331f93538992c6","kind":1,"pubkey":"e8ed3798c6ffebffa08501ac39e271662bfd160f688f94c45d692d8767dd345a","sig":"f76d5ecc8e7de688ac12b9d19edaacdcffb8f0c8fa2a44c00767363af3f04dbc069542ddc5d2f63c94cb5e6ce701589d538cf2db3b1f1211a96596fabb6ecafe","tags":[["e","5fcb28b72cadab2e4cbf7311f4acf5f6d1a99a1c2e642f6b6f0d5518a940f9ec","","mention"],["p","e8ed3798c6ffebffa08501ac39e271662bfd160f688f94c45d692d8767dd345a","","mention"],["t","bitcoin"],["t","bitcoin"]]}]"#).unwrap();
+        if let RelayMessage::Event { event, .. } = message {
+            event.verify().unwrap();
+        } else {
+            panic!("Wrong relay message");
+        }
     }
 
     #[test]
