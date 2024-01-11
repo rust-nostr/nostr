5152ea2928752a89bab871e0fd41fa8e94651041
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-01-11 09:16:08 +0100

    database: update `perf` crate

--- crates/nostr-database/fuzz/perf/src/main.rs
+++ crates/nostr-database/fuzz/perf/src/main.rs
@@ -1,26 +1,43 @@
-use nostr_database::nostr::Event;
-use nostr_database::DatabaseIndexes;
+use std::collections::BTreeSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use nostr_database::nostr::RelayMessage;
+use nostr_database::{DatabaseIndexes, RawEvent};
 use tracing_subscriber::fmt::format::FmtSpan;
-
-mod constants;
-
-use self::constants::EVENTS;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::fmt()
+    tracing_subscriber::fmt()
         .with_span_events(FmtSpan::CLOSE)
         .init();
 
     // Load events
-    let events: Vec<Event> = serde_json::from_str(EVENTS).unwrap();
-    let len = events.len();
-    let events = events.into_iter().map(|e| e.into()).collect();
+    // Open JSON file
+    let file = File::open("./many-events.json").unwrap();
+    let metadata = file.metadata().unwrap();
+    let reader = BufReader::new(file);
+
+    println!("Size: {}", metadata.len());
+
+    // Deserialize events
+    let mut events: BTreeSet<RawEvent> = BTreeSet::new();
+
+    for line in reader.lines() {
+        match line {
+            Ok(line_content) => {
+                if let Ok(RelayMessage::Event { event, .. }) = serde_json::from_str(&line_content) {
+                    events.insert(event.as_ref().into());
+                }
+            }
+            Err(e) => eprintln!("Error reading line: {}", e),
+        }
+    }
 
     // Indexes
     let indexes = DatabaseIndexes::new();
 
     // Bulk load
-    println!("Indexing {len} events");
+    println!("Indexing {} events", events.len());
     indexes.bulk_index(events).await;
 }
