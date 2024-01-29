cce7bdd8b6a369d2e3075565973530a237bc350e
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-01-29 18:53:16 +0100

    js(nostr-sdk): add `relay-service-flags.js` example

---
+++ bindings/nostr-sdk-js/examples/relay-service-flags.js
@@ -0,0 +1,32 @@
+const { Keys, Client, ClientSigner, loadWasmAsync, RelayServiceFlags, initLogger, LogLevel } = require("../");
+
+async function main() {
+    await loadWasmAsync();
+
+    initLogger(LogLevel.info());
+
+    try {
+        // Generate random keys
+        let keys = Keys.generate();
+        let signer = ClientSigner.keys(keys);
+        let client = new Client(signer);
+
+        await client.addRelay("wss://relay.damus.io");
+        await client.connect();
+
+        // This note will be published
+        await client.publishTextNote("My first text note from Nostr SDK WASM!", []);
+
+        // Change relay service flags (remove write permission)
+        let relay = await client.relay("wss://relay.damus.io");
+        let flags = relay.flags();
+        flags.remove(RelayServiceFlags.write()); // Use flags.add(..); to add a flag
+
+        // This note will NOT be published
+        await client.publishTextNote("Trying to send a second note", []);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
