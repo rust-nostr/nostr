19c4415e3b14307a396a7432c33c2063812e66d0
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-02-16 12:23:13 +0100

    Add `nostr-webln`

---
+++ crates/nostr-webln/src/lib.rs
@@ -0,0 +1,81 @@
+// Copyright (c) 2022-2023 Yuki Kishimoto
+// Copyright (c) 2023-2024 Rust Nostr Developers
+// Distributed under the MIT software license
+
+//! WebLN zapper backend for Nostr apps
+
+#![forbid(unsafe_code)]
+#![warn(missing_docs)]
+#![warn(rustdoc::bare_urls)]
+#![allow(unknown_lints, clippy::arc_with_non_send_sync)]
+#![cfg_attr(not(target_arch = "wasm32"), allow(unused))]
+
+pub extern crate nostr_zapper as zapper;
+pub extern crate webln;
+
+use std::ops::Deref;
+
+#[cfg(target_arch = "wasm32")]
+use nostr_zapper::NostrZapper;
+use nostr_zapper::{ZapperBackend, ZapperError};
+use webln::WebLN;
+
+/// [WebLN] zapper
+#[derive(Debug, Clone)]
+pub struct WebLNZapper {
+    inner: WebLN,
+}
+
+impl Deref for WebLNZapper {
+    type Target = WebLN;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl WebLNZapper {
+    /// New [WebLN] zapper
+    ///
+    /// Internally, automatically call `webln.enable()`.
+    pub async fn new() -> Result<Self, ZapperError> {
+        let inner = WebLN::new().map_err(ZapperError::backend)?;
+        inner.enable().await.map_err(ZapperError::backend)?;
+        Ok(Self { inner })
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+macro_rules! impl_nostr_zapper {
+    ({ $($body:tt)* }) => {
+        #[nostr_zapper::async_trait(?Send)]
+        impl NostrZapper for WebLNZapper {
+            type Err = ZapperError;
+
+            $($body)*
+        }
+    };
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+macro_rules! impl_nostr_zapper {
+    ({ $($body:tt)* }) => {
+        impl WebLNZapper {
+            $($body)*
+        }
+    };
+}
+
+impl_nostr_zapper!({
+    fn backend(&self) -> ZapperBackend {
+        ZapperBackend::WebLN
+    }
+
+    async fn pay_invoice(&self, invoice: String) -> Result<(), ZapperError> {
+        self.inner
+            .send_payment(invoice)
+            .await
+            .map_err(ZapperError::backend)?;
+        Ok(())
+    }
+});
