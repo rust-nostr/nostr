6a0324ab7c7f7bab55a2479e130eac397ec2ae25
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-03-11 11:53:54 +0100

    nostr: impl Default for `MessagesLimits` and `EventsLimits`

--- crates/nostr-relay-pool/src/relay/limits.rs
+++ crates/nostr-relay-pool/src/relay/limits.rs
@@ -2,10 +2,10 @@
 // Copyright (c) 2023-2024 Rust Nostr Developers
 // Distributed under the MIT software license
 
-//! Limits
+//! Relay limits
 
 /// Limits
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
 pub struct Limits {
     /// Messages limits
     pub messages: MessagesLimits,
@@ -13,34 +13,35 @@ pub struct Limits {
     pub events: EventsLimits,
 }
 
-impl Default for Limits {
-    fn default() -> Self {
-        Self {
-            messages: MessagesLimits {
-                max_size: 5_250_000,
-            },
-            events: EventsLimits {
-                max_size: 70_000,
-                max_num_tags: 2_000,
-            },
-        }
-    }
-}
-
 /// Messages limits
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct MessagesLimits {
-    /// Maximum size of normalised JSON, in bytes
+    /// Maximum size of normalised JSON, in bytes (default: 5_250_000)
     pub max_size: u32,
+}
+
+impl Default for MessagesLimits {
+    fn default() -> Self {
+        Self {
+            max_size: 5_250_000,
+        }
+    }
 }
 
 /// Events limits
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct EventsLimits {
-    /// Maximum size of normalised JSON, in bytes
+    /// Maximum size of normalised JSON, in bytes (default: 70_000)
     pub max_size: u32,
-    /// Maximum number of tags allowed
+    /// Maximum number of tags allowed (default: 2_000)
     pub max_num_tags: u16,
-    // Maximum size for tag values, in bytes
-    // pub max_tag_val_size: u16,
+}
+
+impl Default for EventsLimits {
+    fn default() -> Self {
+        Self {
+            max_size: 70_000,
+            max_num_tags: 2_000,
+        }
+    }
 }
