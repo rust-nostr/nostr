e471d5015b4f7e28134586e28067f1ca5692268a
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-03-15 11:03:25 +0100

    ffi: fix `Unresolved reference: Cleaner` error when compiling kotlin bindings
    * Bump `gradle` to `8.4`
    * Bump `com.android.tools.build:gradle` to `8.3.0`
    * Bump `compileSdk` to `34`
    * Bump `org.jetbrains.kotlin.android` to `1.9.22`
    * Bump `androidx.appcompat:appcompat` to `1.6.1`
    * Remove `targetSdk`
    * Remove `package` from `AndroidManifest.xml`
    * Remove `org.jetbrains.kotlin:kotlin-stdlib-jdk7` dependency
    * Remove `androidx.core:core-ktx` dependency
    * Add android `namespace`
    * Add `android_cleaner = true` in `uniffi.toml`

--- bindings/nostr-ffi/bindings-android/lib/build.gradle.kts
+++ bindings/nostr-ffi/bindings-android/lib/build.gradle.kts
@@ -3,7 +3,7 @@ val libraryVersion: String by project
 
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android") version "1.6.10"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
     id("maven-publish")
     id("signing")
 }
@@ -14,27 +14,43 @@ repositories {
 }
 
 android {
-    compileSdk = 31
+    namespace = "rust.nostr.protocol"
+    
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 31
+
         consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
-        getByName("release") {
+        release {
             isMinifyEnabled = false
             proguardFiles(file("proguard-android-optimize.txt"), file("proguard-rules.pro"))
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
         }
     }
 }
 
 dependencies {
     implementation("net.java.dev.jna:jna:5.12.0@aar")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7")
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
 }
 
 afterEvaluate {
