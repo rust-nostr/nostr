bf6e0c00e9af8ae7296acaae89fac6e957de11a4
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-03-31 19:18:43 +0200

    sdk: add `limits` example

---
+++ crates/nostr-sdk/examples/limits.rs
@@ -0,0 +1,29 @@
+// Copyright (c) 2022-2023 Yuki Kishimoto
+// Copyright (c) 2023-2024 Rust Nostr Developers
+// Distributed under the MIT software license
+
+use nostr_sdk::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    // Customize relay limits
+    let mut limits = RelayLimits::default();
+    limits.messages.max_size = 10_000;
+    limits.events.max_size = 3_000;
+
+    // Compose options and limits
+    let opts = Options::new().relay_limits(limits);
+    let client = ClientBuilder::default().opts(opts).build();
+
+    // Add relays and connect
+    client
+        .add_relays(["wss://nostr.oxtr.dev", "wss://relay.damus.io"])
+        .await?;
+    client.connect().await;
+
+    // ...
+
+    Ok(())
+}
