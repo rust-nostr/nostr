57d0b004a5c2c2905fcaac37d6d679969f5e0512
Mynima <62206239+RydalWater@users.noreply.github.com>
2024-05-24 13:29:51 +0100

    book: add NIP-05 python examples
    Adjustments based on the review feedback:
    - Removed Deserializing NIP-05
    - Added Verify NIP-05
    - Added Get NIP-05 profile

--- book/snippets/nostr/python/src/nip05.py
+++ book/snippets/nostr/python/src/nip05.py
@@ -1,33 +1,43 @@
-from nostr_protocol import Keys, Metadata, EventBuilder
+from nostr_protocol import Keys, Metadata, EventBuilder, PublicKey, verify_nip05, get_nip05_profile
 
 def nip05():
     # Generate random keys
     keys = Keys.generate()
 
-    # ANCHOR: create-event
+    # ANCHOR: set-metadata
     # Create metadata object with name and NIP05
     metadata_content = Metadata()\
         .set_name("TestName")\
         .set_nip05("TestName@rustNostr.com")
+    # ANCHOR_END: set-metadata
 
     # Build event and assign metadata
     builder = EventBuilder.metadata(metadata_content)
 
     # Signed event (Normal)
-    print("\nCreating Metadata Event:")
+    print("\nCreating NIP-05 Metadata Event:")
     event = builder.to_event(keys)
 
     print(" Event Details:")
     print(f"     Kind      : {event.kind().as_u16()}")
     print(f"     Content   : {event.content()}")
-    # ANCHOR_END: create-event
-
-    # ANCHOR: create-metadata
+    
+    # ANCHOR: verify-nip05
     # Deserialize Metadata from event
-    print("\nDeserializing Metadata Event:")
-    metadata = Metadata().from_json(event.content())
+    print("\nVerify NIP-05:")
+    nip_05 = "yuki@yukikishimoto.com"
+    public_key = PublicKey.parse("npub1drvpzev3syqt0kjrls50050uzf25gehpz9vgdw08hvex7e0vgfeq0eseet")
+    proxy = None
+    if verify_nip05(public_key, nip_05, proxy):
+        print(f"     {nip_05} Verified, for {public_key.to_bech32()}")
+    else:
+        print(f"     Unable to Verifiy NIP-05, for {public_key.to_bech32()}")
+    # ANCHOR_END: verify-nip05
 
-    print(" Metadata Details:")
-    print(f"     Name      : {metadata.get_name()}")
-    print(f"     NIP05     : {metadata.get_nip05()}")
-    # ANCHOR_END: create-metadata
+    #ANCHOR: nip05-profile
+    print("\nProfile NIP-05:")
+    nip_05 = "Rydal@gitlurker.info"
+    profile = get_nip05_profile(nip_05)
+    print(f"     {nip_05} Profile: {profile.to_bech32()}")
+    public_key = PublicKey.parse("npub1zwnx29tj2lnem8wvjcx7avm8l4unswlz6zatk0vxzeu62uqagcash7fhrf")
+    #ANCHOR_END: nip05-profile
