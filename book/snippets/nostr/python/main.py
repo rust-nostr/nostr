98fbe0423724d59bf2eb6cfac5b382c94a5259bb
Mynima <62206239+RydalWater@users.noreply.github.com>
2024-05-25 10:30:00 +0100

    book: add NIP-06 python examples
    Added NIP-06 examples:
    - Derive from seed (24 words)
    - Derive from seed (12 words)
    - Derive from seed (with accounts)
    - Derive from seed (with accounts & passphrase)

--- book/snippets/nostr/python/main.py
+++ book/snippets/nostr/python/main.py
@@ -5,6 +5,7 @@ from src.event.json import event_json
 from src.event.builder import event_builder
 from src.nip01 import nip01
 from src.nip05 import nip05
+from src.nip06 import nip06
 from src.nip44 import nip44
 from src.nip59 import nip59
 
@@ -17,6 +18,7 @@ def main():
     event_builder()
     nip01()
     nip05()
+    nip06()
     nip44()
     nip59()
 
