06586826a0507763d218b8901d6a3f3b330348c1
Mynima <62206239+rydalwater@users.noreply.github.com>
2024-05-28 10:51:22 +0100

    book: add NIP-19 python examples
    Added NIP-19 examples for:
    * Basic (npub, nsec, note)
    * nprofile (encode/decode)
    * nevent (encode/decode)
    * naddr (encode/decode)
    
    Closes https://github.com/rust-nostr/nostr/pull/441
    
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

---
+++ book/snippets/nostr/python/src/nip19.py
@@ -0,0 +1,59 @@
+from nostr_protocol import Keys, EventBuilder, Nip19Profile, Nip19, Nip19Event, Coordinate, Kind, Nip19Enum
+def nip19():
+    keys = Keys.generate()
+
+    print()
+    print("Bare keys and ids (bech32):")
+    # ANCHOR: nip19-npub
+    print(f" Public key: {keys.public_key().to_bech32()}")
+    # ANCHOR_END: nip19-npub
+
+    # ANCHOR: nip19-nsec
+    print(f" Secret key: {keys.secret_key().to_bech32()}")
+    # ANCHOR_END: nip19-nsec
+
+    # ANCHOR: nip19-note
+    event = EventBuilder.text_note("Hello from Rust Nostr Python bindings!", []).to_event(keys)
+    print(f" Event     : {event.id().to_bech32()}")
+    # ANCHOR_END: nip19-note
+
+    print()
+    print("Shareable identifiers with extra metadata (bech32):")
+    # ANCHOR: nip19-nprofile-encode
+    # Create NIP-19 profile including relays data
+    relays = ["wss://relay.damus.io"]
+    nprofile = Nip19Profile(keys.public_key(),relays)
+    print(f" Profile (encoded): {nprofile.to_bech32()}")
+    # ANCHOR_END: nip19-nprofile-encode
+
+    # ANCHOR: nip19-nprofile-decode
+    # Decode NIP-19 profile
+    decode_nprofile = Nip19.from_bech32(nprofile.to_bech32())
+    print(f" Profile (decoded): {decode_nprofile}")
+    # ANCHOR_END: nip19-nprofile-decode
+
+    print()
+    # ANCHOR: nip19-nevent-encode
+    # Create NIP-19 event including author and relays data
+    nevent = Nip19Event(event.id(),keys.public_key(),relays)
+    print(f" Event (encoded): {nevent.to_bech32()}")
+    # ANCHOR_END: nip19-nevent-encode
+
+    # ANCHOR: nip19-nevent-decode
+    # Decode NIP-19 event
+    decode_nevent = Nip19.from_bech32(nevent.to_bech32())
+    print(f" Event (decoded): {decode_nevent}")
+    # ANCHOR_END: nip19-nevent-decode
+  
+    print()
+    # ANCHOR: nip19-naddr-encode
+    # Create NIP-19 coordinate
+    coord = Coordinate(Kind(0),keys.public_key())
+    print(f" Coordinate (encoded): {coord.to_bech32()}")
+    # ANCHOR_END: nip19-naddr-encode
+
+    # ANCHOR: nip19-naddr-decode
+    # Decode NIP-19 coordinate
+    decode_coord = Nip19.from_bech32(coord.to_bech32())
+    print(f" Coordinate (decoded): {decode_coord}")
+    # ANCHOR_END: nip19-naddr-decode
