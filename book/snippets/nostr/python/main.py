541d5c2132c10e087c53405ab506fb22492ec3e4
Mynima <62206239+rydalwater@users.noreply.github.com>
2024-05-29 10:53:15 +0100

    book: add NIP-21 python examples
    * Added nip-21 python examples (including: npub, note, nprofile, nevent, naddr)
    * Removed unnecessary class import reference in nip19.py
    * Corrected terminology for book where "function" was used instead of "method"
    
    Closes https://github.com/rust-nostr/nostr/pull/443
    
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- book/snippets/nostr/python/main.py
+++ book/snippets/nostr/python/main.py
@@ -7,6 +7,7 @@ from src.nip01 import nip01
 from src.nip05 import nip05
 from src.nip06 import nip06
 from src.nip19 import nip19
+from src.nip21 import nip21
 from src.nip44 import nip44
 from src.nip59 import nip59
 
@@ -21,6 +22,7 @@ def main():
     nip05()
     nip06()
     nip19()
+    nip21()
     nip44()
     nip59()
 
---
+++ book/snippets/nostr/python/src/nip21.py
@@ -0,0 +1,85 @@
+from nostr_protocol import Keys, PublicKey, EventBuilder, EventId, Nip21, Nip19Profile, Nip19Event, Kind, Coordinate
+
+
+def nip21():
+    print()
+    print("Nostr URIs:")
+    # ANCHOR: npub
+    keys = Keys.generate()
+
+    # URI npub
+    pk_uri = keys.public_key().to_nostr_uri()
+    print(f" Public key (URI):    {pk_uri}")
+
+    # bech32 npub
+    pk_parse = Nip21.parse(pk_uri)
+    if pk_parse.as_enum().is_pubkey():
+        pk_bech32 = PublicKey.from_nostr_uri(pk_uri).to_bech32()
+        print(f" Public key (bech32): {pk_bech32}")
+    # ANCHOR_END: npub
+
+    print()
+
+    # ANCHOR: note
+    event = EventBuilder.text_note("Hello from Rust Nostr Python bindings!", []).to_event(keys)
+
+    # URI note
+    note_uri = event.id().to_nostr_uri()
+    print(f" Event (URI):    {note_uri}")
+
+    # bech32 note
+    note_pasre = Nip21.parse(note_uri)
+    if note_pasre.as_enum().is_note():
+        event_bech32 = EventId.from_nostr_uri(note_uri).to_bech32()
+        print(f" Event (bech32): {event_bech32}")
+    # ANCHOR_END: note
+
+    print()
+
+    # ANCHOR: nprofile
+    relays = ["wss://relay.damus.io"]
+    nprofile = Nip19Profile(keys.public_key(), relays)
+
+    # URI nprofile
+    nprofile_uri = nprofile.to_nostr_uri()
+    print(f" Profile (URI):    {nprofile_uri}")
+
+    # bech32 nprofile
+    nprofile_parse = Nip21.parse(nprofile_uri)
+    if nprofile_parse.as_enum().is_profile():
+        nprofile_bech32 = Nip19Profile.from_nostr_uri(nprofile_uri).to_bech32()
+        print(f" Profile (bech32): {nprofile_bech32}")
+    # ANCHOR_END: nprofile
+
+    print()
+
+    # ANCHOR: nevent
+    relays = ["wss://relay.damus.io"]
+    nevent = Nip19Event(event.id(), keys.public_key(), relays)
+
+    # URI nevent
+    nevent_uri = nevent.to_nostr_uri()
+    print(f" Event (URI):    {nevent_uri}")
+
+    # bech32 nevent
+    nevent_parse = Nip21.parse(nevent_uri)
+    if nevent_parse.as_enum().is_event():
+        nevent_bech32 = Nip19Event.from_nostr_uri(nevent_uri).to_bech32()
+        print(f" Event (bech32): {nevent_bech32}")
+    # ANCHOR_END: nevent
+
+    print()
+
+    # ANCHOR: naddr
+    coord = Coordinate(Kind(0), keys.public_key())
+
+    # URI naddr
+    coord_uri = coord.to_nostr_uri()
+    print(f" Coordinate (URI):    {coord_uri}")
+
+    # bech32 naddr
+    coord_parse = Nip21.parse(coord_uri)
+    if coord_parse.as_enum().is_coord():
+        coord_bech32 = Coordinate.from_nostr_uri(coord_uri).to_bech32()
+        print(f" Coordinate (bech32): {coord_bech32}")
+    # ANCHOR_END: naddr
