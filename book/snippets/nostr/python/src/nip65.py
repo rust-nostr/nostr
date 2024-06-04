2292034120e2b1df9064529b2a1b43bc8abe7275
Mynima <62206239+RydalWater@users.noreply.github.com>
2024-06-04 11:54:07 +0100

    book: add NIP-65 python example
    Expanded example to include both simple and complex methods for constructing the NIP-65 relay-metadata events.

--- book/snippets/nostr/python/src/nip65.py
+++ book/snippets/nostr/python/src/nip65.py
@@ -6,7 +6,25 @@ def nip65():
 
     print()
     print("Relay Metadata:")
-    # ANCHOR: relay-metadata
+    # ANCHOR: relay-metadata-simple
+    # Create relay dictionary
+    relays_dict = {
+        "wss://relay.damus.io": RelayMetadata.READ,
+        "wss://relay.primal.net": RelayMetadata.WRITE,
+        "wss://relay.nostr.band": None
+    }
+    
+    # Build/sign event
+    builder = EventBuilder.relay_list(relays_dict)
+    event = builder.to_event(keys)
+
+    # Print event as json
+    print(f" Event: {event.as_json()}")
+    # ANCHOR_END: relay-metadata-simple
+
+    print()
+
+    # ANCHOR: relay-metadata-custom
     # Create relay metadata tags
     tag1 = Tag.relay_metadata("wss://relay.damus.io", RelayMetadata.READ)
     tag2 = Tag.relay_metadata("wss://relay.primal.net", RelayMetadata.WRITE)
@@ -21,4 +39,4 @@ def nip65():
 
     # Print event as json
     print(f" Event: {event.as_json()}")
-    # ANCHOR_END: relay-metadata
+    # ANCHOR_END: relay-metadata-custom
