5891a59e76dc956491d762ba6db12837d1db810e
Mynima <62206239+RydalWater@users.noreply.github.com>
2024-06-13 21:30:46 +0100

    book: add filters python examples
    Removed additional function call in filters.py used during development

--- book/snippets/nostr/python/src/filters.py
+++ book/snippets/nostr/python/src/filters.py
@@ -153,5 +153,3 @@ def filters():
     print(f"     Event match for filter: {f.match_event(event)}")
     print(f"     Event2 match for filter: {f.match_event(event2)}")
     # ANCHOR_END: other-match
-
-filters()
