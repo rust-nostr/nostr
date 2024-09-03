7bbc323515e2357d533870da9e90c15a6864c4d4
Mynima <62206239+RydalWater@users.noreply.github.com>
2024-09-03 20:23:53 +0100

    book: add NIP01 JavaScript examples
    * Updated `06-nip01.md` to include JS code blocks
    * Added JS examples equivalent to Python code
    
    Closes https://github.com/rust-nostr/nostr/pull/560

--- book/snippets/nostr/js/index.js
+++ book/snippets/nostr/js/index.js
@@ -2,6 +2,7 @@ const keys = require("./src/keys");
 const eventJson = require("./src/event/json");
 const eventBuilder = require("./src/event/builder");
 const relayMessageJson = require("./src/messages/relay");
+const nip01 = require("./src/nip01");
 const nip44 = require("./src/nip44");
 const nip59 = require("./src/nip59");
 
@@ -15,6 +16,7 @@ eventBuilder.eventBuilder();
 
 relayMessageJson.relayMessageJson();
 
+nip01.run();
 nip44.run();
 
 nip59.run();
