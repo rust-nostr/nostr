e342916d718b8f36d62ae07b1dd5b9131b3eaea8
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-09-07 18:19:01 -0400

    ffi(sdk): remove `ndb` and `sqlite` from default features
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- bindings/nostr-sdk-ffi/bindings-python/scripts/generate-windows.sh
+++ bindings/nostr-sdk-ffi/bindings-python/scripts/generate-windows.sh
@@ -7,7 +7,7 @@ pip install --user -r requirements.txt
 echo "Generating native binaries..."
 rustup target add x86_64-pc-windows-msvc
 # TODO: enable all features
-cargo build --release --target x86_64-pc-windows-msvc --no-default-features --features sqlite
+cargo build --release --target x86_64-pc-windows-msvc --features lmdb
 
 echo "Generating nostr_sdk.py..."
 cd ../
