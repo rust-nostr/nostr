ca7687201a9ccad599da0a8291756f0c65b67294
Mynima <62206239+rydalwater@users.noreply.github.com>
2024-09-10 17:36:18 +0100

    book: add NIP05 & NIP19 JavaScript examples
    * Updated index.js to include nip05.js and nip19.js
    * Added js examples to snippets for nip05 and nip19
    * Updated book src to embed code snippets into existing text
    
    Closes https://github.com/rust-nostr/nostr/pull/569
    
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- book/snippets/nostr/js/index.js
+++ book/snippets/nostr/js/index.js
@@ -3,20 +3,28 @@ const eventJson = require("./src/event/json");
 const eventBuilder = require("./src/event/builder");
 const relayMessageJson = require("./src/messages/relay");
 const nip01 = require("./src/nip01");
+const nip05 = require("./src/nip05");
+const nip19 = require("./src/nip19");
 const nip44 = require("./src/nip44");
 const nip59 = require("./src/nip59");
 
-// Keys
-keys.generate();
-keys.restore();
-keys.vanity();
+async function main() {
+    // Keys
+    keys.generate();
+    keys.restore();
+    keys.vanity();
 
-eventJson.eventJson();
-eventBuilder.eventBuilder();
+    eventJson.eventJson();
+    eventBuilder.eventBuilder();
 
-relayMessageJson.relayMessageJson();
+    relayMessageJson.relayMessageJson();
 
-nip01.run();
-nip44.run();
+    nip01.run();
+    await nip05.run();
+    nip19.run();
+    nip44.run();
 
-nip59.run();
+    nip59.run();
+}
+
+main();
---
+++ book/snippets/nostr/js/src/nip05.js
@@ -0,0 +1,37 @@
+const { loadWasmAsync, PublicKey, Metadata, verifyNip05, getNip05Profile } = require("@rust-nostr/nostr");
+
+async function run() {
+    // Load WASM
+    await loadWasmAsync();
+
+    console.log();
+    // ANCHOR: set-metadata
+    // Create metadata object with name and NIP05
+    let metadata = new Metadata()
+        .name("TestName")
+        .nip05("TestName@rustNostr.com");
+    // ANCHOR_END: set-metadata
+
+    console.log();
+    // ANCHOR: verify-nip05
+    console.log("Verify NIP-05:");
+    let nip05 = "Rydal@gitlurker.info";
+    let publicKey = PublicKey.parse("npub1zwnx29tj2lnem8wvjcx7avm8l4unswlz6zatk0vxzeu62uqagcash7fhrf");
+    if (await verifyNip05(publicKey, nip05)) {
+        console.log(`     '${nip05}' verified, for ${publicKey.toBech32()}`);
+    } else {
+        console.log(`     Unable to verify NIP-05, for ${publicKey.toBech32()}`);
+    };
+    // ANCHOR_END: verify-nip05
+
+    console.log();
+
+    // ANCHOR: nip05-profile
+    console.log("Get NIP-05 profile:");
+    let nip_05 = "yuki@yukikishimoto.com";
+    let profile = await getNip05Profile(nip_05);
+    console.log(`     ${nip_05} Public key: ${profile.publicKey().toBech32()}`);
+    // ANCHOR_END: nip05-profile
+}
+
+module.exports.run = run;
