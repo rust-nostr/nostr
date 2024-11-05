42547f987439d30a17e4f51c79bf5ffd5b4bc501
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-11-05 11:18:24 +0100

    flutter: fix build script
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- bindings/nostr-sdk-flutter/rust/build.rs
+++ bindings/nostr-sdk-flutter/rust/build.rs
@@ -2,18 +2,33 @@
 // Copyright (c) 2023-2024 Rust Nostr Developers
 // Distributed under the MIT software license
 
+use std::process::{Command, Stdio};
+
 use lib_flutter_rust_bridge_codegen::codegen;
 use lib_flutter_rust_bridge_codegen::codegen::Config;
 
 fn main() {
     println!("cargo:rerun-if-changed=src/api");
 
-    // Execute code generator with auto-detected config
-    codegen::generate(
-        Config::from_config_file("../flutter_rust_bridge.yaml")
-            .unwrap()
-            .unwrap(),
-        Default::default(),
-    )
-    .unwrap();
+    if is_flutter_installed() {
+        // Execute code generator with auto-detected config
+        codegen::generate(
+            Config::from_config_file("../flutter_rust_bridge.yaml")
+                .unwrap()
+                .unwrap(),
+            Default::default(),
+        )
+        .unwrap();
+    } else {
+        eprintln!("Warning: flutter not installed.");
+    }
+}
+
+fn is_flutter_installed() -> bool {
+    let output = Command::new("flutter")
+        .arg("--version")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status();
+    matches!(output, Ok(status) if status.success())
 }
