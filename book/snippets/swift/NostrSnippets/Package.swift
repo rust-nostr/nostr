d9dde7101d1d0c5d9b0d29047b813d3e32a425f5
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-11-16 12:16:59 +0100

    book: enable Swift Package tests
    * Downgrade `swift-tools-version` to 5.5
    
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- book/snippets/swift/NostrSnippets/Package.swift
+++ book/snippets/swift/NostrSnippets/Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "NostrSnippets",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v12)],
     dependencies: [
-        .package(url: "https://github.com/rust-nostr/nostr-sdk-swift", from:"0.36.0")
+        .package(url: "https://github.com/rust-nostr/nostr-sdk-swift", from: "0.36.0")
     ],
     targets: [
         .executableTarget(
--- book/snippets/swift/NostrSnippets/Sources/Keys.swift
+++ book/snippets/swift/NostrSnippets/Sources/Keys.swift
@@ -1,6 +1,16 @@
 import NostrSDK
 import Foundation
 
-func keys() {
-    // TODO
+func keys() throws {
+    // ANCHOR: generate
+    let keys = Keys.generate()
+    let publicKey = keys.publicKey()
+    let secretKey = keys.secretKey()
+
+    print("Public key (hex): \(publicKey.toHex())")
+    print("Secret key (hex): \(secretKey.toHex())")
+
+    print("Public key (bech32): \(try publicKey.toBech32())")
+    print("Secret key (bech32): \(try secretKey.toBech32())")
+    // ANCHOR_END: generate
 }
