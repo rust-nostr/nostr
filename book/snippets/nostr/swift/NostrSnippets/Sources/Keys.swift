0d300b68c6180a60fbdde6821f1533bbef1a089d
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-11-16 12:16:59 +0100

    book: test Swift
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- book/snippets/nostr/swift/NostrSnippets/Sources/Keys.swift
+++ book/snippets/nostr/swift/NostrSnippets/Sources/Keys.swift
@@ -2,5 +2,12 @@ import NostrSDK
 import Foundation
 
 func keys() {
-    // TODO
+    // ANCHOR: generate
+    let keys = Keys.generate()
+    print("Public key (hex): \(keys.publicKey.toHex())")
+    print("Secret key (hex): \(keys.secretKey.toHex())")
+
+    print("Public key (bech32): \(keys.publicKey.toBech32())")
+    print("Secret key (bech32): \(keys.secretKey.toBech32())")
+    // ANCHOR_END: generate
 }
