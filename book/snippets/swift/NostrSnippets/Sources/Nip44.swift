74f389314093b636f7ea65b05e4072b112019458
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-11-27 18:42:49 +0100

    book: add `Nip44.swift` snippet
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

---
+++ book/snippets/swift/NostrSnippets/Sources/Nip44.swift
@@ -0,0 +1,14 @@
+import Foundation
+import NostrSDK
+
+func nip44() throws {
+    let keys = Keys.generate()
+
+    let publicKey = try PublicKey.fromHex(hex: "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798")
+
+    let ciphertext = try nip44Encrypt(secretKey: keys.secretKey(), publicKey: publicKey, content: "my message", version: Nip44Version.v2)
+    print("Encrypted: \(ciphertext)");
+
+    let plaintext = try nip44Decrypt(secretKey: keys.secretKey(), publicKey: publicKey, payload: ciphertext)
+    print("Decrypted: \(plaintext)");
+}
--- book/snippets/swift/NostrSnippets/Sources/nip44.swift
+++
@@ -1,6 +0,0 @@
-import NostrSDK
-import Foundation
-
-func nip44() {
-    // TODO
-}
