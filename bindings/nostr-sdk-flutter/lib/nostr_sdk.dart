75e579c07e5174954ade0ba634fdf9eb4b305c8f
ethicnology <ethicnology@pm.me>
2024-12-02 11:25:13 -0500

    refactor: export the bindings

--- bindings/nostr-sdk-flutter/lib/nostr_sdk.dart
+++ bindings/nostr-sdk-flutter/lib/nostr_sdk.dart
@@ -1,16 +1,19 @@
 // The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
+library nostr_sdk;
 
-//
+export 'src/rust/frb_generated.dart' show NostrSdk;
+
+export 'src/rust/api/protocol/event.dart';
+export 'src/rust/api/protocol/event/tag.dart';
+export 'src/rust/api/protocol/key.dart';
+export 'src/rust/api/protocol/key/public_key.dart';
+export 'src/rust/api/protocol/key/secret_key.dart';
+export 'src/rust/api/client.dart';
+
 // import 'nostr_sdk_platform_interface.dart';
-//
+
 // class NostrSdk {
 //   Future<String?> getPlatformVersion() {
 //     return NostrSdkPlatform.instance.getPlatformVersion();
 //   }
 // }
-//
-
-library nostr_sdk;
-
-export 'src/rust/api/simple.dart';
-export 'src/rust/frb_generated.dart' show RustLib;
