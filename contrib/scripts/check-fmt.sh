d8b4ea7d34e92ca4fc9a5423e9937ca76072075c
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-12-11 12:49:24 -0500

    diff --git a/contrib/scripts/check-fmt.sh b/contrib/scripts/check-fmt.sh index af6bafc1..eedc1ec4 100755 --- a/contrib/scripts/check-fmt.sh +++ b/contrib/scripts/check-fmt.sh @@ -2,7 +2,7 @@
     set -euo pipefail
    
    -version="nightly-2024-11-18"
    +version=${2:-nightly-2024-11-18}
     flags=""
    
     # Check if "check" is passed as an argument
    @@ -11,7 +11,8 @@ if [[ "$#" -gt 0 && "$1" == "check" ]]; then
     fi
    
     # Install toolchain
    -cargo +$version --version || (rustup install $version && rustup component add rustfmt --toolchain $version)
    +cargo +$version --version 2>/dev/null || (rustup install $version && rustup component add rustfmt --toolchain $version)
    
     # Check workspace crates
    -cargo +$version fmt --all -- --config format_code_in_doc_comments=true $flags
    +cargo +$version fmt --all -- --config format_code_in_doc_comments=true $flags 2>/dev/null || \
    +rustup run $version cargo fmt --all -- --config format_code_in_doc_comments=true $flags

--- contrib/scripts/check-fmt.sh
+++ contrib/scripts/check-fmt.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-version="nightly-2024-11-18"
+version=${2:-nightly-2024-11-18}
 flags=""
 
 # Check if "check" is passed as an argument
@@ -11,7 +11,8 @@ if [[ "$#" -gt 0 && "$1" == "check" ]]; then
 fi
 
 # Install toolchain
-cargo +$version --version || (rustup install $version && rustup component add rustfmt --toolchain $version)
+cargo +$version --version 2>/dev/null || (rustup install $version && rustup component add rustfmt --toolchain $version)
 
 # Check workspace crates
-cargo +$version fmt --all -- --config format_code_in_doc_comments=true $flags
+cargo +$version fmt --all -- --config format_code_in_doc_comments=true $flags 2>/dev/null || \
+rustup run $version cargo fmt --all -- --config format_code_in_doc_comments=true $flags
