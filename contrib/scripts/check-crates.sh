9ad327f022fd9b331a6b7058ce0e650584575256
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-01-03 09:20:39 +0100

    Bump `async-wsocket` to 0dfeee76
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- contrib/scripts/check-crates.sh
+++ contrib/scripts/check-crates.sh
@@ -51,7 +51,8 @@ buildargs=(
 )
 
 skip_msrv=(
-    "-p nostr-cli"     # MSRV: 1.74.0
+    "-p nostr-sdk --features tor"     # MSRV: 1.77.0
+    "-p nostr-cli"                    # MSRV: 1.74.0
 )
 
 for arg in "${buildargs[@]}";
