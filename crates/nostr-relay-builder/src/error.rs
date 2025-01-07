82f465963333fdb71f685ef658651951f856bdfd
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-01-07 15:08:47 +0100

    relay-builder: remove `thiserror` dep
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- crates/nostr-relay-builder/src/error.rs
+++ crates/nostr-relay-builder/src/error.rs
@@ -4,20 +4,42 @@
 
 //! Relay builder error
 
-use std::io;
+use std::{fmt, io};
 
 #[cfg(feature = "tor")]
 use async_wsocket::native::tor;
-use thiserror::Error;
 
 /// Relay builder error
-#[derive(Debug, Error)]
+#[derive(Debug)]
 pub enum Error {
     /// I/O error
-    #[error(transparent)]
-    IO(#[from] io::Error),
+    IO(io::Error),
     /// Tor error
     #[cfg(feature = "tor")]
-    #[error(transparent)]
-    Tor(#[from] tor::Error),
+    Tor(tor::Error),
+}
+
+impl std::error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::IO(e) => write!(f, "{e}"),
+            #[cfg(feature = "tor")]
+            Self::Tor(e) => write!(f, "{e}"),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Self::IO(e)
+    }
+}
+
+#[cfg(feature = "tor")]
+impl From<tor::Error> for Error {
+    fn from(e: tor::Error) -> Self {
+        Self::Tor(e)
+    }
 }
