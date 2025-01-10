9384a0b871f0781625a75a6cdbac2f90308c9c9e
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-01-10 14:36:28 +0100

    ffi: temp remove tests
    New uniffi version doesn't find cdylib for tests
    
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- bindings/nostr-sdk-ffi/tests/test_generated_bindings.rs
+++
@@ -1,5 +0,0 @@
-uniffi::build_foreign_language_testcases!(
-    "tests/test_equality.py",
-    "tests/test_nip05.py",
-    "tests/test_to_string.py",
-);
--- bindings/nostr-sdk-ffi/tests/test_to_string.py
+++
@@ -1,8 +0,0 @@
-from nostr_sdk import *
-
-addr = "30023:aa4fc8665f5696e33db7e1a572e3b0f5b3d615837b0f362dcb1c8068b098c7b4:ipsum"
-coordinate = Coordinate.parse(addr)
-assert addr == coordinate.__str__()
-
-t_kind = TagKind.NONCE()
-assert tag_kind_to_string(t_kind) == "nonce"
