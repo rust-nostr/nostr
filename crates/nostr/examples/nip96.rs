bc0d6b89afbee56c15e8eb1cb93bd159f8d888df
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-01-13 11:56:50 +0100

    nostr: add NIP96 example
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

---
+++ crates/nostr/examples/nip96.rs
@@ -0,0 +1,24 @@
+// Copyright (c) 2022-2023 Yuki Kishimoto
+// Copyright (c) 2023-2024 Rust Nostr Developers
+// Distributed under the MIT software license
+
+use nostr::prelude::*;
+
+const FILE: &[u8] = include_bytes!("../../../LICENSE");
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let keys = Keys::parse("nsec1j4c6269y9w0q2er2xjw8sv2ehyrtfxq3jwgdlxj6qfn8z4gjsq5qfvfk99")?;
+
+    let server_url: Url = Url::parse("https://NostrMedia.com")?;
+
+    let config: ServerConfig = nip96::get_server_config(server_url, None).await?;
+
+    let file_data: Vec<u8> = FILE.to_vec();
+
+    // Upload
+    let url: Url = nip96::upload_data(&keys, &config, file_data, None, None).await?;
+    println!("File uploaded: {url}");
+
+    Ok(())
+}
