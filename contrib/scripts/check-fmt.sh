4506345ad07c0449993b14f27f30cacb9092a253
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-03-12 13:41:14 +0100

    contrib: separate rustfmt installation from toolchain setup
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- contrib/scripts/check-fmt.sh
+++ contrib/scripts/check-fmt.sh
@@ -11,7 +11,10 @@ if [[ "$#" -gt 0 && "$1" == "check" ]]; then
 fi
 
 # Install toolchain
-cargo +$version --version || (rustup install $version && rustup component add rustfmt --toolchain $version)
+cargo +$version --version || rustup install $version
+
+# Install rustfmt
+cargo +$version fmt --version || rustup component add rustfmt --toolchain $version
 
 # Check workspace crates
 cargo +$version fmt --all -- --config format_code_in_doc_comments=true $flags
