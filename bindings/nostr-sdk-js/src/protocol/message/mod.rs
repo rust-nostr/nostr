c266adbc97eff0b64efeb204f3fa0423706196b0
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-03-19 09:26:38 +0100

    Move JS bindings to a dedicated repo
    Move nostr-sdk-js to https://github.com/rust-nostr/nostr-sdk-js
    
    Closes https://github.com/rust-nostr/nostr/issues/813
    
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- bindings/nostr-sdk-js/src/protocol/message/mod.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022-2023 Yuki Kishimoto
-// Copyright (c) 2023-2025 Rust Nostr Developers
-// Distributed under the MIT software license
-
-mod client;
-mod relay;
-
-pub use self::client::JsClientMessage;
-pub use self::relay::JsRelayMessage;
--- contrib/scripts/check-bindings.sh
+++ contrib/scripts/check-bindings.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 # Check bindings
 buildargs=(
     "-p nostr-sdk-ffi"
-    "-p nostr-sdk-js --target wasm32-unknown-unknown"
 )
 
 for arg in "${buildargs[@]}"; do
@@ -13,10 +12,7 @@ for arg in "${buildargs[@]}"; do
 
     cargo check $arg
 
-    if [[ $arg != *"--target wasm32-unknown-unknown"* ]];
-    then
-        cargo test $arg
-    fi
+    cargo test $arg
 
     cargo clippy $arg -- -D warnings
 
