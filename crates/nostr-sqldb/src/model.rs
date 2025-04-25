bd86247c93741b368e638787605dd5437181bab2
tompro <office@protom.eu>
2025-04-25 10:31:12 +0200

    Reuse FlatBufferBuilder

--- crates/nostr-sqldb/src/model.rs
+++ crates/nostr-sqldb/src/model.rs
@@ -1,3 +1,5 @@
+use std::sync::{Mutex, OnceLock};
+
 use diesel::prelude::*;
 use nostr::event::Event;
 use nostr_database::{DatabaseError, FlatBufferBuilder, FlatBufferEncode};
@@ -37,19 +39,32 @@ pub struct EventDataDb {
 impl TryFrom<&Event> for EventDataDb {
     type Error = DatabaseError;
     fn try_from(value: &Event) -> Result<Self, Self::Error> {
-        let serialized = value.encode(&mut FlatBufferBuilder::new()).to_vec();
         Ok(Self {
             event: EventDb {
                 id: value.id.to_string(),
                 pubkey: value.pubkey.to_string(),
                 created_at: value.created_at.as_u64() as i64,
                 kind: value.kind.as_u16() as i64,
-                payload: serialized,
+                payload: encode_payload(value),
                 signature: value.sig.to_string(),
                 deleted: false,
             },
             tags: extract_tags(value),
         })
+    }
+}
+
+fn encode_payload(value: &Event) -> Vec<u8> {
+    static FB_BUILDER: OnceLock<Mutex<FlatBufferBuilder>> = OnceLock::new();
+    match FB_BUILDER
+        .get_or_init(|| Mutex::new(FlatBufferBuilder::new()))
+        .lock()
+    {
+        Ok(mut fb_builder) => {
+            fb_builder.reset();
+            value.encode(&mut fb_builder).to_vec()
+        }
+        Err(_) => value.encode(&mut FlatBufferBuilder::new()).to_vec(),
     }
 }
 
