318ea9d55fe79c04dcfb1e91c264fb1eddc6121c
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-08-15 10:18:22 +0200

    nostr: add additional optional fields to `MakeInvoiceResponse`
    Add `description`, `description_hash`, `preimage`, `amount`, `created_at` and `expires_at` optional fields to `MakeInvoiceResponse` struct.
    
    Co-authored-by: daywalker90 <8257956+daywalker90@users.noreply.github.com>
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- crates/nostr/src/nips/nip47.rs
+++ crates/nostr/src/nips/nip47.rs
@@ -662,12 +662,33 @@ pub struct PayKeysendResponse {
 }
 
 /// Make Invoice Response
+//
+// NOTE: don't add `type`, `state` or `fees_paid`, as they don't make sense here.
+//
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct MakeInvoiceResponse {
     /// Bolt 11 invoice
     pub invoice: String,
     /// Invoice's payment hash
     pub payment_hash: String,
+    /// Invoice's description
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    /// Invoice's description hash
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description_hash: Option<String>,
+    /// Payment preimage
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub preimage: Option<String>,
+    /// Amount in msats.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub amount: Option<u64>,
+    /// Creation timestamp in seconds since epoch
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub created_at: Option<Timestamp>,
+    /// Expiration timestamp in seconds since epoch
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub expires_at: Option<Timestamp>,
 }
 
 /// Lookup Invoice Response
