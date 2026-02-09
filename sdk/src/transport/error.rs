092181c4561358e9d258fa745b130481749382da
Yuki Kishimoto <yukikishimoto@protonmail.com>
2026-02-09 16:21:42 +0100

    sdk: update TransportError
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- sdk/src/transport/error.rs
+++ sdk/src/transport/error.rs
@@ -1,14 +1,13 @@
-// Copyright (c) 2022-2023 Yuki Kishimoto
-// Copyright (c) 2023-2025 Rust Nostr Developers
-// Distributed under the MIT software license
+//! Transport error
 
-//! Transport Error
-
-use core::fmt;
+use std::fmt;
+use std::io::{self, ErrorKind};
 
 /// Transport Error
 #[derive(Debug)]
 pub enum TransportError {
+    /// I/O error
+    IO(io::Error),
     /// An error happened in the underlying backend.
     Backend(Box<dyn std::error::Error + Send + Sync>),
 }
@@ -18,20 +17,33 @@ impl std::error::Error for TransportError {}
 impl fmt::Display for TransportError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            Self::IO(e) => e.fmt(f),
             Self::Backend(e) => e.fmt(f),
         }
     }
 }
 
+impl From<io::Error> for TransportError {
+    fn from(e: io::Error) -> Self {
+        Self::IO(e)
+    }
+}
+
 impl TransportError {
+    /// Timeout error
+    #[inline]
+    pub fn timeout() -> Self {
+        Self::IO(io::Error::new(ErrorKind::TimedOut, "timeout"))
+    }
+
     /// Create a new backend error
     ///
     /// Shorthand for `Error::Backend(Box::new(error))`.
     #[inline]
     pub fn backend<E>(error: E) -> Self
     where
-        E: std::error::Error + Send + Sync + 'static,
+        E: Into<Box<dyn std::error::Error + Send + Sync>>,
     {
-        Self::Backend(Box::new(error))
+        Self::Backend(error.into())
     }
 }
